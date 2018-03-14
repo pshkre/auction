35c60c2005cde38c29fcc8727034a20f03024805
pshkre <pshkre@gmail.com>
2018-03-14 20:15:51 +0800

    change the style
    change the style of website

--- blockchain_action/auction/buildAuction/views.py
+++ blockchain_action/auction/buildAuction/views.py
@@ -2,6 +2,4 @@ from django.shortcuts import render
 
 def create_contract(request):
     #create = {'name':'參與拍賣!',}
-    if 'a' in request.GET:
-        print(request.GET['a'])
     return render(request, 'buildAuction/create_contract.html')
