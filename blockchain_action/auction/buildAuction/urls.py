1aafabbf484ee9edcd197a74d876ddc1713f1904
pshkre <pshkre@gmail.com>
2018-03-20 16:24:47 +0800

    show your contract detail
    show your contract detail

--- blockchain_action/auction/buildAuction/urls.py
+++ blockchain_action/auction/buildAuction/urls.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', views.create_contract , name='create_contract'),
+     url(r'^test.html', TemplateView.as_view(template_name="buildAuction/test.html")),
 ]
