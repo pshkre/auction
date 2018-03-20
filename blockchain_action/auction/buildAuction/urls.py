3e0b9244a96fadba18f11f021ef73e3c8a3ce078
pshkre <pshkre@gmail.com>
2018-03-20 16:45:27 +0800

    fix conflicts
    fix conflicts

--- blockchain_action/auction/buildAuction/urls.py
+++ blockchain_action/auction/buildAuction/urls.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', views.create_contract , name='create_contract'),
+    url(r'^detail.html', TemplateView.as_view(template_name="buildAuction/detail.html")),
 ]
